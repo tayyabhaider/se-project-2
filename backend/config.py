dfac3f8ec25d7ed0cb6bfe6dc94e8dbf72cd74f8
Tayyab <zaiditayyab175@gmail.com>
2025-05-04 10:16:19 +0500

    redis connected

--- backend/config.py
+++ backend/config.py
@@ -13,6 +13,7 @@ REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))  # Default to 6379 if not set
 FLASK_ENV = os.getenv("FLASK_ENV", "development")
 VITE_API_URL = os.getenv("VITE_API_URL")
+REDIS_URL = os.getenv("REDIS_URL")
 
 
 
@@ -35,6 +36,7 @@ users_collection = db["users"]
 summary_collection = db["summary"]
 media_collection = db["media"]
 media_fs = gridfs.GridFS(db)
-redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
+# redis_client = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0, decode_responses=True)
+redis_client = redis.StrictRedis.from_url(REDIS_URL, decode_responses=True)
 
 
